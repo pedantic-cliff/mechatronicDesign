c4bf7efd94f50b9d0e511ae8b73eae735e14fe9b
Chris Leaf <chris@thinkero.us>
2014-02-17 00:22:31 -0500

    Interrupts working from button

--- inc/main.h
+++ inc/main.h
@@ -2,6 +2,9 @@
 #include "stm32f4xx_rcc.h"
 #include "stm32f4xx_gpio.h"
 #include "stm32f4xx_tim.h"
+#include "stm32f4xx_syscfg.h"
+#include "stm32f4xx_exti.h"
+#include "misc.h"
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
@@ -12,4 +15,5 @@ void loop();
 void delay();
 
 void initButton();
+void initLEDs();
 #endif //_MAIN_H_
--- src/main.c
+++ src/main.c
@@ -2,28 +2,26 @@
 #include "pid.h"
 
 /* leds in the board will fade */
+typedef enum { PID, ACCEL } STATE; 
+volatile STATE state; 
 
-const uint16_t USER_BUTTON = GPIO_Pin_0;
 int main(void) {
   init();
   pid_pos(500); 
   do {
-//    loop();
+    //    loop();
   } while (1);
 }
 
 void init() {
+  state = PID; 
   initButton();
+  initLEDs();
   initPID(1.f, 0.f, 0.f);
 }
 
 int lastButtonStatus = RESET;
 void loop() {
-
-  uint8_t currentButtonStatus = GPIO_ReadInputDataBit(GPIOA, USER_BUTTON);
-  if (lastButtonStatus != currentButtonStatus && currentButtonStatus != RESET) {
-  }
-  lastButtonStatus = currentButtonStatus;
 
   delay(4);
 }
@@ -36,11 +34,67 @@ void delay(uint32_t ms) {
 }
 
 void initButton(void) {
+  // Helper Typedefs
+
+  // Connect EXTI to Button Pin 
+  SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, GPIO_PinSource0); 
+
+  //Configure EXTI (Interrupt MUX) 
+  EXTI_InitTypeDef  EXTI_InitStructure; 
+
+  EXTI_InitStructure.EXTI_Line      = EXTI_Line0;          // Line select
+  EXTI_InitStructure.EXTI_Mode      = EXTI_Mode_Interrupt; // 
+  EXTI_InitStructure.EXTI_Trigger   = EXTI_Trigger_Rising; // Trigger Mode
+  EXTI_InitStructure.EXTI_LineCmd   = ENABLE;
+  EXTI_Init(&EXTI_InitStructure); 
+
+  //Configure NVIC 
+  NVIC_InitTypeDef  NVIC_InitStructure;
+
+  NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;        //Channel Select
+  // Set interrupt priority 
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x0F; // Lowest
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x0F;        // Lowest
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure); 
+
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
   GPIO_InitTypeDef gpio;
   GPIO_StructInit(&gpio);
   gpio.GPIO_Mode = GPIO_Mode_IN;
-  gpio.GPIO_Pin = USER_BUTTON;
-  GPIO_Init(GPIOA, &gpio);
+  gpio.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  gpio.GPIO_Pin = GPIO_Pin_0;
+  GPIO_Init(GPIOD, &gpio);
+
+}
+
+// Interrupt Channel 0 handler
+void EXTI0_IRQHandler(void){
+  if(EXTI_GetITStatus(EXTI_Line0) != RESET){
+    switch(state){
+      case PID: 
+        state = ACCEL;
+        GPIO_SetBits(GPIOD,GPIO_Pin_12); 
+        GPIO_ResetBits(GPIOD,GPIO_Pin_14); 
+        break;
+      case ACCEL: 
+        state = PID; 
+        GPIO_SetBits(GPIOD,GPIO_Pin_14); 
+        GPIO_ResetBits(GPIOD,GPIO_Pin_12); 
+        break;
+    }
+  }
+  EXTI_ClearITPendingBit(EXTI_Line0); 
+}
+
+void initLEDs() {
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+  GPIO_InitTypeDef gpio;
+  GPIO_StructInit(&gpio);
+  gpio.GPIO_Mode = GPIO_Mode_OUT;
+  gpio.GPIO_Pin  = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+  GPIO_Init(GPIOD, &gpio);
 }
 
